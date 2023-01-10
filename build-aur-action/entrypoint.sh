2222be320680b6188216736ca716f54a562967d5
Chauncey <iflygo@outlook.com>
2023-01-10 18:01:00 +0800

    update

--- build-aur-action/entrypoint.sh
+++ build-aur-action/entrypoint.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
 pkgname=$1
+
+pacman-key --init
+pacman -Sy --noconfirm
+
+if [ ! -z "$INPUT_PREINSTALLPKGS" ]; then
+    pacman -Syu --noconfirm "$INPUT_PREINSTALLPKGS"
+fi
+
 git clone "https://aur.archlinux.org/$pkgname.git"
 cd "$pkgname"
 echo $PWD
