ce9d23b0c518cf69d7f5d0b8fdf623e44b2ee3ec
Chauncey <iflygo@outlook.com>
2023-01-11 16:35:11 +0800

    fix

--- build-aur-action/entrypoint.sh
+++ build-aur-action/entrypoint.sh
@@ -6,6 +6,7 @@ pacman-key --init
 pacman -Sy --noconfirm
 
 git clone "https://aur.archlinux.org/$pkgname.git" && cd $pkgname
+git rev-parse --short HEAD
 
 if [ ! -z "$INPUT_PREINSTALLPKGS" ]; then
     pacman -Syu --noconfirm "$INPUT_PREINSTALLPKGS"
