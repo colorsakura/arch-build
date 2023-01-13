25ca8219c6a1f390b184fea3af73fda0c8ada068
Chauncey <iflygo@outlook.com>
2023-01-13 22:13:00 +0800

    add: build from local aur pkg

--- build-aur-action/entrypoint.sh
+++ build-aur-action/entrypoint.sh
@@ -1,17 +1,34 @@
 #!/bin/bash
 
 pkgname=$1
+localpath=$PWD
 
 pacman-key --init
 pacman -Sy --noconfirm
 
-git clone "https://aur.archlinux.org/$pkgname.git" && cd $pkgname
-git rev-parse --short HEAD
-
-if [ ! -z "$INPUT_PREINSTALLPKGS" ]; then
-    pacman -Syu --noconfirm "$INPUT_PREINSTALLPKGS"
-    makepkg -f --noconfirm
+if [ ! -z "$INPUT_LOCALSOURCE" ]; then
+    echo "build from local source"
+    mkdir ./buildpkgs
+    cd ./pkgfiles/$pkgname
 else
-    makepkg -sf --noconfirm
+    echo "build from aur package"
+    git clone "https://aur.archlinux.org/$pkgname.git" && cd $pkgname
+    git rev-parse --short HEAD
 fi
 
+if [ ! -z "$INPUT_PREINSTALLPKGS" ]; then
+    echo "manu"
+    pacman -Syu --noconfirm "$INPUT_PREINSTALLPKGS"
+    makepkg -f --noconfirm
+    if [ ! -z "$INPUT_LOCALSOURCE" ]; then
+        cp *.pkg.tar.zst $localpath/buildpkgs/
+    fi
+else
+    echo "auto"
+    makepkg -sf --noconfirm
+    if [ ! -z "$INPUT_LOCALSOURCE" ]; then
+        cp *.pkg.tar.zst $localpath/buildpkgs/
+    fi
+fi
+
+# vim: set sw=4 et:
