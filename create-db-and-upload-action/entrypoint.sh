bca629200d1f0f770465061f18479cc413eab2d8
Xiang Chen <32534829+colorsakura@users.noreply.github.com>
2024-01-13 00:22:35 +0800

    Update entrypoint.sh

--- create-db-and-upload-action/entrypoint.sh
+++ create-db-and-upload-action/entrypoint.sh
@@ -20,9 +20,9 @@ if [ ! -f ~/.config/rclone/rclone.conf ]; then
 	echo "drive_id=$RCLONE_ONEDRIVE_DRIVE_ID" >>~/.config/rclone/rclone.conf
 fi
 
-if [ ! -z "$GPG_PRIVATE_KEY" ]; then
-	echo "$GPG_PRIVATE_KEY" | gpg --import
-fi
+#if [ ! -z "$GPG_PRIVATE_KEY" ]; then
+#	echo "$GPG_PRIVATE_KEY" | gpg --import
+#fi
 
 cd upload_packages || exit 1
 
@@ -31,17 +31,17 @@ python3 $init_path/create-db-and-upload-action/sync.py
 #rm "./${repo_name:?}.db.tar.gz"
 #rm "./${repo_name:?}.files.tar.gz"
 
-if [ ! -z "$GPG_PRIVATE_KEY" ]; then
-	packages=("*.tar.zst")
-	for name in $packages; do
-		if [ ! -z "$GPG_PASSWORD" ]; then
-			gpg --batch --passphrase $GPG_PASSWORD --detach-sig --yes $name
-			repo-add --verify --sign "./${repo_name:?}.db.tar.gz" ./*.tar.zst
-			ls -al
-		else
-			gpg --detach-sig --yes $name
-			repo-add --verify --sign "./${repo_name:?}.db.tar.gz" ./*.tar.zst
-		fi
-	done
-fi
+# if [ ! -z "$GPG_PRIVATE_KEY" ]; then
+# 	packages=("*.tar.zst")
+# 	for name in $packages; do
+# 		if [ ! -z "$GPG_PASSWORD" ]; then
+# 			gpg --batch --passphrase $GPG_PASSWORD --detach-sig --yes $name
+# 			repo-add --verify --sign "./${repo_name:?}.db.tar.gz" ./*.tar.zst
+# 			ls -al
+# 		else
+# 			gpg --detach-sig --yes $name
+# 			repo-add --verify --sign "./${repo_name:?}.db.tar.gz" ./*.tar.zst
+# 		fi
+# 	done
+# fi
 rclone copy ./ "onedrive:${dest_path:?}" --copy-links
